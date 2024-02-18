b073e77fe3c584f7626de67d0619d34bb30c9326
Koovenn <educanaless@gmail.com>
2024-02-18 22:51:06 +0000

    Faltan 2 Endpoints

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,84 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const prisma = require("../prisma");
 
-router.get('/', (req, res) => {})
+// Mostrar todos los libros
+router.get("/", async (req, res) => {
+  try {
+    const allBooks = await prisma.libro.findMany();
+    res.json(allBooks);
+  } catch (e) {
+    console.log(e);
+    res.json("Server error");
+  }
+});
+
+// Combinar tablas de Libros y Ventas usando el ISBN (devolvía null si la ponía debajo de las otras rutas)
+router.get("/with-sales", async (req, res) => {
+  try {
+    const booksWithSales = await prisma.venta.findMany({
+      include: {
+        Libro: {
+          select: {
+            Titulo: true,
+            Autor: true,
+            Precio: true,
+          },
+        },
+      },
+    });
+    res.json(booksWithSales);
+  } catch (e) {
+    console.log(e);
+    res.json("Server error");
+  }
+});
+
+// Mostrar un libro en particular
+router.get("/:isbn", async (req, res) => {
+  try {
+    const oneBook = await prisma.libro.findUnique({
+      where: {
+        ISBN: req.params.isbn,
+      },
+    });
+    res.json(oneBook);
+  } catch (e) {
+    console.log(e);
+    res.json("Server error");
+  }
+});
+
+// Mostrar todos los libros de un autor en particular
+router.get("/author/:author", async (req, res) => {
+  try {
+    const oneAuthorBooks = await prisma.libro.findMany({
+      where: {
+        Autor: req.params.author,
+      },
+    });
+    res.json(oneAuthorBooks);
+  } catch (e) {
+    console.log(e);
+    res.json("Server error");
+  }
+});
+
+// Mostrar todos los libros con un precio superior a 20
+router.get("/price/:price", async (req, res) => {
+  try {
+    const expensiveBooks = await prisma.libro.findMany({
+      where: {
+        Precio: {
+          gt: Number(req.params.price),
+        },
+      },
+    });
+    res.json(expensiveBooks);
+  } catch (e) {
+    console.log(e);
+    res.json("Server error");
+  }
+});
 
 module.exports = router;
--- routes/sales.js
+++ routes/sales.js
@@ -1,6 +1,46 @@
 const express = require("express");
 const router = express.Router();
+const prisma = require("../prisma");
 
-router.get("/", async (req, res) => {});
+// Mostrar todas las ventas
+router.get("/", async (req, res) => {
+  try {
+    const allSales = await prisma.venta.findMany();
+    res.json(allSales);
+  } catch (e) {
+    console.log(e);
+    res.json("Server error");
+  }
+});
+
+// Mostrar una venta en particular
+router.get("/:id", async (req, res) => {
+  try {
+    const oneSale = await prisma.venta.findUnique({
+      where: {
+        ID_Venta: Number(req.params.id),
+      },
+    });
+    res.json(oneSale);
+  } catch (e) {
+    console.log(e);
+    res.json("Server error");
+  }
+});
+
+// Mostrar todas las ventas de un libro en particular
+router.get("/book/:isbn", async (req, res) => {
+    try {
+      const oneBookSales = await prisma.venta.findMany({
+        where: {
+          ISBN: req.params.isbn,
+        },
+      });
+      res.json(oneBookSales);
+    } catch (e) {
+      console.log(e);
+      res.json("Server error");
+    }
+  });
 
 module.exports = router;
